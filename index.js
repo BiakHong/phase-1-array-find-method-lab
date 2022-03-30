0725e1cba56f4c7cf6a77bac779643003957b5d4
Lian <biakhong@gmail.com>
2022-03-30 12:08:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(arg){
+    function callBack(year){
+        return year.result === "W";
+    }
+    const winningYear = arg.find(callBack);
+        if(winningYear){
+            return winningYear.year
+        }
+}
